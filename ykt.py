f5383f3ed651a2ae4a875d3646cfed4bf61490d5
GwanSY <gwansy02@163.com>
2024-12-10 14:33:40 +0800

    1

--- ykt.py
+++ ykt.py
@@ -103,7 +103,6 @@ def ssxx():
                     response = session.get(url)
 
                     JSON = json.loads(response.text)
-
                     break
                 else:
                     print(f"输入错误，请输入一个介于 {min_value} 和 {max_value} 之间的课程编号。")
@@ -115,94 +114,182 @@ def ssxx():
         exit(-1)
 
     url = 'https://www.yuketang.cn/c27/online_courseware/xty/kls/pub_news/' + str(
-        JSON['data']['activities'][1]['courseware_id']) + '/'
+        JSON['data']['activities'][0]['courseware_id']) + '/'
     headers = {
         'xtbz': 'ykt',
         'classroom-id': str(classroom_id)
     }
     response = session.get(url, headers=headers)
-
+    print(response)
     JSON = json.loads(response.text)
+    # print(JSON)
     c_course_id = str(JSON['data']['course_id'])
+    # print(c_course_id)
     s_id = str(JSON['data']['s_id'])
+    # print(s_id)
 
     for i in range(len(JSON['data']['content_info'])):
-        print("正在观看----" + JSON['data']['c_short_name'] + " 第" + str(i) + "章" + "----共找到" + str(
-            len(JSON['data']['content_info'][i]['section_list'])) + "个视频。")
-        for j in range(len(JSON['data']['content_info'][i]['section_list'])):
-            cards_id = '0'
+        if (len(JSON['data']['content_info'][i]['section_list'])):
+            print("正在观看----" + JSON['data']['c_short_name'] + " 第" + str(i+1) + "章" + "----共找到" + str(
+                len(JSON['data']['content_info'][i]['section_list'])) + "个视频。")
+            for j in range(len(JSON['data']['content_info'][i]['section_list'])):
+                # print(len(JSON['data']['content_info'][i]['section_list']))
+                for leaf_list in range(len(JSON['data']['content_info'][i]['section_list'][j]['leaf_list'])):
+                    cards_id = '0'
+                    if JSON['data']['content_info'][i]['section_list'][j]['leaf_list'][leaf_list]['leaf_type'] == 0:
+                        # 0改成1
+                        video_id = str(JSON['data']['content_info'][i]['section_list'][j]['leaf_list'][leaf_list]['id'])
+                        # print(video_id)
+                        url = 'https://www.yuketang.cn/mooc-api/v1/lms/learn/leaf_info/' + classroom_id + '/' + video_id + '/'
+                        response = session.get(url=url, headers=headers)
 
-            video_id = str(JSON['data']['content_info'][i]['section_list'][j]['leaf_list'][0]['id'])
+                        JSON_TEMP = json.loads(response.text)
 
-            url = 'https://www.yuketang.cn/mooc-api/v1/lms/learn/leaf_info/' + classroom_id + '/' + video_id + '/'
-            response = session.get(url=url, headers=headers)
+                        ccid = JSON_TEMP['data']['content_info']['media']['ccid']
+                        d = JSON_TEMP['data']['content_info']['media']['duration']
 
-            JSON_TEMP = json.loads(response.text)
+                        v = str(JSON_TEMP['data']['id'])
+                        u = str(JSON_TEMP['data']['user_id'])
+                        timestamp_ms = int(time.time() * 1000)
+                        url = "https://www.yuketang.cn/video-log/get_video_watch_progress/?cid=" + c_course_id + "&user_id=" + u + "&classroom_id=" + classroom_id + "&video_type=video&vtype=rate&video_id=" + video_id + "&snapshot=1"
+                        response_new = session.get(url=url, headers=headers)
+                        JSON_NEW = json.loads(response_new.text)
+                        if d == 0:
+                            url = "https://www.yuketang.cn/video-log/get_video_watch_progress/?cid=" + c_course_id + "&user_id=" + u + "&classroom_id=" + classroom_id + "&video_type=video&vtype=rate&video_id=" + video_id + "&snapshot=1"
+                            response_new = session.get(url=url, headers=headers)
+                            JSON_NEW = json.loads(response_new.text)
+                            try:
+                                d = int(JSON_NEW[video_id]['video_length'])
+                            except Exception as e:
+                                pass
 
-            ccid = JSON_TEMP['data']['content_info']['media']['ccid']
-            d = JSON_TEMP['data']['content_info']['media']['duration']
+                        try:
+                            sunci = JSON_NEW['data'][video_id]['completed']
+                        except Exception as e:
+                            sunci = 0
 
-            v = str(JSON_TEMP['data']['id'])
-            u = str(JSON_TEMP['data']['user_id'])
-            timestamp_ms = int(time.time() * 1000)
-            url = "https://www.yuketang.cn/video-log/get_video_watch_progress/?cid=" + c_course_id + "&user_id=" + u + "&classroom_id=" + classroom_id + "&video_type=video&vtype=rate&video_id=" + video_id + "&snapshot=1"
-            response_new = session.get(url=url, headers=headers)
-            JSON_NEW = json.loads(response_new.text)
-            if d == 0:
-                url = "https://www.yuketang.cn/video-log/get_video_watch_progress/?cid=" + c_course_id + "&user_id=" + u + "&classroom_id=" + classroom_id + "&video_type=video&vtype=rate&video_id=" + video_id + "&snapshot=1"
-                response_new = session.get(url=url, headers=headers)
-                JSON_NEW = json.loads(response_new.text)
-                try:
-                    d = int(JSON_NEW[video_id]['video_length'])
-                except Exception as e:
-                    pass
+                        while sunci != 1:
+                            for k in range(25):
+                                time.sleep(0.5)
+                                print("正在观看第" + str(i) + "章 第" + str(j + 1) + "个视频----当前进度：" + str(4 * (k + 1)) + "%")
+                                url = 'https://www.yuketang.cn/video-log/heartbeat/'
+                                data = '{"heart_data":[{"i":5,"et":"heartbeat","p":"web","n":"ali-cdn.xuetangx.com","lob":"ykt","cp":' + str(
+                                    d * (1 + k) / 25) + ',"fp":100,"tp":100,"sp":5,"ts":"' + str(timestamp_ms + d * (
+                                        1 + k) * 2500) + '","u":' + u + ',"uip":"","c":' + c_course_id + ',"v":' + v + ',"skuid":' + str(
+                                    s_id) + ',"classroomid":"' + classroom_id + '","cc":"' + ccid + '","d":' + str(
+                                    d) + ',"pg":"' + video_id + '_x33v","sq":11,"t":"video","cards_id":0,"slide":0,"v_url":""}]}'
 
-            try:
-                sunci = JSON_NEW['data'][video_id]['completed']
-            except Exception as e:
-                sunci = 0
+                                headers1 = {
+                                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.42',
+                                    'Content-Type': 'application/json',
+                                    'authority': 'changjiang.yuketang.cn',
+                                    'method': 'GET',
+                                    'path': '/v2/api/web/courses/list?identity=2',
+                                    'referer': 'https://changjiang.yuketang.cn/v2/web/personal/info',
+                                    'sec-fetch-dest': 'empty',
+                                    'sec-fetch-mode': 'cors',
+                                    'sec-fetch-site': 'same-origin',
+                                    # 'university-id':'2727',
+                                }
 
-            while sunci != 1:
-                for k in range(25):
-                    time.sleep(0.6)
-                    print("正在观看第" + str(i) + "章 第" + str(j + 1) + "个视频----当前进度：" + str(4 * (k + 1)) + "%")
-                    url = 'https://www.yuketang.cn/video-log/heartbeat/'
-                    data = '{"heart_data":[{"i":5,"et":"heartbeat","p":"web","n":"ali-cdn.xuetangx.com","lob":"ykt","cp":' + str(
-                        d * (1 + k) / 25) + ',"fp":100,"tp":100,"sp":5,"ts":"' + str(timestamp_ms + d * (
-                            1 + k) * 2500) + '","u":' + u + ',"uip":"","c":' + c_course_id + ',"v":' + v + ',"skuid":' + str(
-                        s_id) + ',"classroomid":"' + classroom_id + '","cc":"' + ccid + '","d":' + str(
-                        d) + ',"pg":"' + video_id + '_x33v","sq":11,"t":"video","cards_id":0,"slide":0,"v_url":""}]}'
+                                response = session.post(url=url, data=data, headers=headers1)
 
-                    headers1 = {
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.42',
-                        'Content-Type': 'application/json',
-                        'authority': 'changjiang.yuketang.cn',
-                        'method': 'GET',
-                        'path': '/v2/api/web/courses/list?identity=2',
-                        'referer': 'https://changjiang.yuketang.cn/v2/web/personal/info',
-                        'sec-fetch-dest': 'empty',
-                        'sec-fetch-mode': 'cors',
-                        'sec-fetch-site': 'same-origin',
-                        # 'university-id':'2727',
-                    }
+                                url = "https://www.yuketang.cn/video-log/get_video_watch_progress/?cid=" + c_course_id + "&user_id=" + u + "&classroom_id=" + classroom_id + "&video_type=video&vtype=rate&video_id=" + video_id + "&snapshot=1"
+                                response_new = session.get(url=url, headers=headers)
+                                JSON_NEW = json.loads(response_new.text)
+                                has_watched = JSON_NEW['data'][video_id]['watch_length']
+                                if d == 0:
+                                    d = int(JSON_NEW[video_id]['video_length'])
 
-                    response = session.post(url=url, data=data, headers=headers1)
 
-                    url = "https://www.yuketang.cn/video-log/get_video_watch_progress/?cid=" + c_course_id + "&user_id=" + u + "&classroom_id=" + classroom_id + "&video_type=video&vtype=rate&video_id=" + video_id + "&snapshot=1"
-                    response_new = session.get(url=url, headers=headers)
-                    JSON_NEW = json.loads(response_new.text)
-                    has_watched = JSON_NEW['data'][video_id]['watch_length']
-                    if d == 0:
-                        d = int(JSON_NEW[video_id]['video_length'])
-                    
+                                try:
+                                    sunci = JSON_NEW['data'][video_id]['completed']
+                                except Exception as e:
+                                    sunci = 0
+                                if sunci == 1:
+                                    break
+        else:
+            for i in range(len(JSON['data']['content_info'])):
+                print("正在观看----" + JSON['data']['c_short_name'] + " 第" + str(i+1) + "章" + "----共找到" + str(
+                    len(JSON['data']['content_info'][i]['leaf_list'])) + "个视频。")
+                for j in range(len(JSON['data']['content_info'][i]['leaf_list'])):
+                    # print(len(JSON['data']['content_info'][i]['leaf_list']))
+                    cards_id = '0'
+                    if JSON['data']['content_info'][i]['leaf_list'][j]['leaf_type'] == 0:
+                        # 0改成1
+                        video_id = str(
+                            JSON['data']['content_info'][i]['leaf_list'][j]['id'])
+                        print(video_id)
+                        url = 'https://www.yuketang.cn/mooc-api/v1/lms/learn/leaf_info/' + classroom_id + '/' + video_id + '/'
+                        response = session.get(url=url, headers=headers)
 
-                    try:
-                        sunci = JSON_NEW['data'][video_id]['completed']
-                    except Exception as e:
-                        sunci = 0
-                    if sunci == 1:
-                        break
-    print("这门课看完了啊！ 孙辞期待与您的下次相遇！")
+                        JSON_TEMP = json.loads(response.text)
+
+                        ccid = JSON_TEMP['data']['content_info']['media']['ccid']
+                        d = JSON_TEMP['data']['content_info']['media']['duration']
+
+                        v = str(JSON_TEMP['data']['id'])
+                        u = str(JSON_TEMP['data']['user_id'])
+                        timestamp_ms = int(time.time() * 1000)
+                        url = "https://www.yuketang.cn/video-log/get_video_watch_progress/?cid=" + c_course_id + "&user_id=" + u + "&classroom_id=" + classroom_id + "&video_type=video&vtype=rate&video_id=" + video_id + "&snapshot=1"
+                        response_new = session.get(url=url, headers=headers)
+                        JSON_NEW = json.loads(response_new.text)
+                        if d == 0:
+                            url = "https://www.yuketang.cn/video-log/get_video_watch_progress/?cid=" + c_course_id + "&user_id=" + u + "&classroom_id=" + classroom_id + "&video_type=video&vtype=rate&video_id=" + video_id + "&snapshot=1"
+                            response_new = session.get(url=url, headers=headers)
+                            JSON_NEW = json.loads(response_new.text)
+                            try:
+                                d = int(JSON_NEW[video_id]['video_length'])
+                            except Exception as e:
+                                pass
+
+                        try:
+                            sunci = JSON_NEW['data'][video_id]['completed']
+                        except Exception as e:
+                            sunci = 0
+
+                        while sunci != 1:
+                            for k in range(25):
+                                time.sleep(0.5)
+                                print("正在观看第" + str(i) + "章 第" + str(j + 1) + "个视频----当前进度：" + str(
+                                    4 * (k + 1)) + "%")
+                                url = 'https://www.yuketang.cn/video-log/heartbeat/'
+                                data = '{"heart_data":[{"i":5,"et":"heartbeat","p":"web","n":"ali-cdn.xuetangx.com","lob":"ykt","cp":' + str(
+                                    d * (1 + k) / 25) + ',"fp":100,"tp":100,"sp":5,"ts":"' + str(
+                                    timestamp_ms + d * (
+                                            1 + k) * 2500) + '","u":' + u + ',"uip":"","c":' + c_course_id + ',"v":' + v + ',"skuid":' + str(
+                                    s_id) + ',"classroomid":"' + classroom_id + '","cc":"' + ccid + '","d":' + str(
+                                    d) + ',"pg":"' + video_id + '_x33v","sq":11,"t":"video","cards_id":0,"slide":0,"v_url":""}]}'
+
+                                headers1 = {
+                                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.42',
+                                    'Content-Type': 'application/json',
+                                    'authority': 'changjiang.yuketang.cn',
+                                    'method': 'GET',
+                                    'path': '/v2/api/web/courses/list?identity=2',
+                                    'referer': 'https://changjiang.yuketang.cn/v2/web/personal/info',
+                                    'sec-fetch-dest': 'empty',
+                                    'sec-fetch-mode': 'cors',
+                                    'sec-fetch-site': 'same-origin',
+                                    # 'university-id':'2727',
+                                }
+
+                                response = session.post(url=url, data=data, headers=headers1)
+
+                                url = "https://www.yuketang.cn/video-log/get_video_watch_progress/?cid=" + c_course_id + "&user_id=" + u + "&classroom_id=" + classroom_id + "&video_type=video&vtype=rate&video_id=" + video_id + "&snapshot=1"
+                                response_new = session.get(url=url, headers=headers)
+                                JSON_NEW = json.loads(response_new.text)
+                                has_watched = JSON_NEW['data'][video_id]['watch_length']
+                                if d == 0:
+                                    d = int(JSON_NEW[video_id]['video_length'])
+
+                                try:
+                                    sunci = JSON_NEW['data'][video_id]['completed']
+                                except Exception as e:
+                                    sunci = 0
+                                if sunci == 1:
+                                    break
+    print("这门课看完了啊！ R_0Ce@N期待与您的下次相遇！")
 
 
 
